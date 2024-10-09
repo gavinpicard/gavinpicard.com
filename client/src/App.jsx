4b8420fe73485bc6f10c6c15c6937e0be9012d28
gavinpicard <gavinlpicard@gmail.com>
2024-10-09 19:48:38 -0400

    prep

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,21 +21,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Hi, I'm Gavin Picard</h1>
+        <p>Here is a placeholder for what will eventually be my bio</p>
+        <div>
+          <div>
+          </div>
+          <div>
+          </div>
+          <div>
+          </div>
+        </div>
         {
           array.map((fruit, index) => (
             <div key={index}>
